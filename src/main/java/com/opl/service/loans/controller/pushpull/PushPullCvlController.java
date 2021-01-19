0ee8ed77fbe83bb834f41d67eb2372f0d0945b0a
yashwant.menaria <yashwant.menaria@CWDT092.corp.capitaworld.com>
2021-01-19 17:30:11 +0530

    package change

--- src/main/java/com/opl/service/loans/controller/pushpull/PushPullCvlController.java
+++ src/main/java/com/opl/service/loans/controller/pushpull/PushPullCvlController.java
@@ -1,52 +1,24 @@
 
 package com.opl.service.loans.controller.pushpull;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import com.opl.mudra.api.user.model.FpProfileBasicDetailRequest;
-import com.opl.mudra.api.user.model.UserResponse;
-import com.opl.mudra.api.user.model.UsersRequest;
-import com.opl.mudra.client.users.UsersClient;
-import com.opl.mudra.api.loans.model.AdminPanelLoanDetailsResponse;
-import com.opl.mudra.api.loans.model.FrameRequest;
-import com.opl.mudra.api.loans.model.LoanApplicationDetailsForSp;
-import com.opl.mudra.api.loans.model.LoanApplicationRequest;
-import com.opl.mudra.api.loans.model.LoanDisbursementRequest;
-import com.opl.mudra.api.loans.model.LoanPanCheckRequest;
-import com.opl.mudra.api.loans.model.LoanSanctionRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.opl.mudra.api.loans.model.LoansResponse;
-import com.opl.mudra.api.loans.model.PaymentRequest;
-import com.opl.mudra.api.loans.model.common.AutoFillOneFormDetailRequest;
-import com.opl.mudra.api.loans.model.common.ChatDetails;
-import com.opl.mudra.api.loans.model.common.DisbursementRequest;
-import com.opl.mudra.api.loans.model.common.EkycRequest;
-import com.opl.mudra.api.loans.model.mobile.MobileLoanRequest;
 import com.opl.mudra.api.loans.utils.CommonUtils;
-import com.opl.mudra.api.matchengine.model.ProposalMappingRequest;
-import com.opl.mudra.api.payment.model.GatewayResponse;
-import com.opl.mudra.client.matchengine.ProposalDetailsClient;
-import com.opl.service.loans.domain.fundseeker.ApplicationProposalMapping;
-import com.opl.service.loans.domain.sidbi.PushPullRequest;
+import com.opl.service.loans.model.pushpull.PushPullRequest;
 import com.opl.service.loans.repository.common.LoanRepository;
-import com.opl.service.loans.service.common.AutoFillOneFormDetailService;
-import com.opl.service.loans.service.fundseeker.corporate.ApplicationProposalMappingService;
-import com.opl.service.loans.service.fundseeker.corporate.LoanApplicationService;
-import com.opl.service.loans.service.fundseeker.corporate.PushPullApplicationService;
-import com.opl.service.loans.service.sanction.LoanDisbursementService;
-import com.opl.service.loans.service.sanction.LoanSanctionService;
-import com.opl.service.loans.utils.CommonDocumentUtils;
-import com.opl.service.loans.utils.CommonUtility;
+import com.opl.service.loans.service.pushpull.PushPullApplicationService;
 
 @RestController
 @RequestMapping("/loan_application")
