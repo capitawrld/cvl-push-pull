84bd6d3beafe03157026b0b9f5fa80e0199ecb94
jaybCW <jay.barbhaya@onlinepsbloans.com>
2021-04-13 17:29:00 +0530

    -- cors filter change

--- src/main/java/com/opl/service/loans/config/CorsFilter.java
+++ src/main/java/com/opl/service/loans/config/CorsFilter.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.opl.utils.utility.CorsFilterUtils;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,21 +19,9 @@ public class CorsFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "*");
-		response.setHeader("Access-Control-Max-Age", "-1");
-		response.setHeader("Access-Control-Allow-Headers",
-			    "Content-Type, Access-Control-Allow-Headers, X-Requested-With,tk_ac,ur_cu,tk_rc,tk_lg,req_auth,*");
-		response.setHeader("X-Frame-Options","ALLOW-FROM *.capitaworld.com");
-		HttpServletRequest request = ((HttpServletRequest) req);
-		String origin = request.getHeader("origin");
-		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-			if((origin.contains("http://localhost:") || origin.contains("https://localhost:"))){
-				response.setStatus(HttpServletResponse.SC_OK);
-			}else {
-				response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-			}
-		} else {
+		HttpServletRequest request = (HttpServletRequest) req;
+		boolean result = CorsFilterUtils.setHeader(response, request);
+		if (!result) {
 			chain.doFilter(req, res);
 		}
 	}
